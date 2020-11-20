2314224ccc094cd02d74d207adad20cc0b04d4ad
Wandy Santana <sntwandy@gmail.com>
2020-11-20 00:38:17 -0400

    Using directives v-if, v-else-if, v-show

---
+++ js/index.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: "#root",
+  data() {
+    return {
+      name: "Bitcoin",
+      img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+      changePercent: 0,
+    };
+  },
+});
